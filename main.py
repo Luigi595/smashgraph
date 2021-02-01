acee66f011e4df30f547625646c572004d68e9c7
Luigi595 <32586912+Luigi595@users.noreply.github.com>
2021-02-01 00:39:18 -0600

    prototype adjacent matrix function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sqlite3
 import igraph as ig
 import cairo
+import pandas as pd
+import numpy as np
 
 #I don't understand Cairo, but run this with python 3.6
 
@@ -27,12 +29,32 @@ c. execute("SELECT * FROM players WHERE player_id='1004'")
 c. execute("SELECT * FROM sets")
 last20=c.fetchmany(size=20)
 
-players=[]
-for match in last20:
-    players.append(match[4])
-    players.append(match[5])
 
-print(players)
+c.execute("SELECT tag FROM players")
+players=c.fetchall()
+print(len(players))
+
+#c.execute("SELECT game_data FROM sets")
+#print(c.fetchall()[-2:])
+
+def adjancency():
+    count=0
+    adjMatrix= pd.DataFrame(np.zeros((len(players), len(players))), index=players, columns=players)
+    for player in players:
+        count+=1
+        if count%100==0: print(count)
+        c.execute("SELECT p1_score-p2_score, p1_id, p2_id FROM sets WHERE winner_id=?", (player))
+        temp=c.fetchall()
+        for game in temp:
+            if game[1]==player:
+                adjMatrix.loc[player,game[2]]+= game[0]
+            elif game[2]== player:
+                adjMatrix.loc[player,game[1]]+= game[0]
+    adjMatrix.to_csv("out.csv")
+
+
+adjancency()
+
 c.close()
 conn.close()
 
