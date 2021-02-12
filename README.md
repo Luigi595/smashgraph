b47669ed47461de2dbc34337bb9abc0bd771bc53
Luigi595 <32586912+Luigi595@users.noreply.github.com>
2021-02-11 23:36:59 -0600

    Core ranking function and test runs

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 ### A Graph Theory approach to ranking Melee players
 
 ## Prerequisites
-* Python 3.6
-* Igraph
-* Cairo or cairocffi (If you want to render the graph)
+Pretty standard data science libraries
+* Python 3.6+
+* Numpy
+* Pandas
 
 ## Data
-The data for this project comes from [smashdata.gg](https://smashdata.gg/)
+The data for this project comes from [smashdata.gg](https://smashdata.gg/) 
 
 ## Preprocessing
 DQs are removed from the data.  
@@ -23,4 +24,12 @@ The weight on each arc is determined by the following formula
 
 ## Problems (Opportunities if you're feeling positive)
 
-Some of the tags in the database are duplicated, while a part of those belong to different people e.g. King, some of them don't e.g. Aniolas. I haven't found a way to scan and solve this problem for 4743 instances of duplicated tags.
+Some of the tags in the database are duplicated, while a part of those belong to different people e.g. King, some of them don't e.g. Aniolas. I haven't found a way to scan and solve this problem for 4743 instances of duplicated tags. Considering that most top players have unique tags, I decided to merge records of identical tags.
+
+`numpy.linalg.eigs(A)` may produce an error related to the LAPACK wrapper. Apparently this is a problem with numpy 1.19.4 on Windows.
+
+Not all tournaments are created equal, the original model was used to rank college football so that assumption was fair. On the other hand, it's not the same to defeat Axe at a weekly than to do it at Genesis. As a result of this, without any modifications to the model, we find that Falgoat, an unranked falco main ranks #36 in this model's 2019 Ranking, having defeated Zain at Blast Off! #31, a tournament with 30 entrants.
+
+Incomplete data even in recent years. Stango was ranked 47th on the 2019 MPGR, while ranking 148th here. Upon inspection, we noticed that the database misses his run at Fight Pitt 9, which was described as the "run of his career". Or AbsentPage, whose only 2019 record in the database is his Genesis 6 run.
+
+When all eigenvalues are zero, alpha is not limited. This happens when the graph has no loops of the form Mang0 beats Zain who beats Leffen who beats Mang0 or longer. Such seems to be the case of the 2020 season.
--- main.py
+++ main.py
@@ -1,71 +1,74 @@
 import sqlite3
-import igraph as ig
-import cairo
 import pandas as pd
 import numpy as np
 
-#I don't understand Cairo, but run this with python 3.6
-
 conn = sqlite3.connect('melee_player_database.db')
-
 c = conn.cursor()
 
 c.execute("SELECT * FROM sqlite_master")
-tables=c.fetchall()
-
-for i in tables:
+for i in c:
     print(i)
     print("\n")
 
-#Get 20 sets
-#Vertex are players
-#Direction goes to winner
 
-
-#Add new column? Nevermind the first line, can't drop column on sqlite
-#c.execute("ALTER TABLE sets DROP COLUMN loser_id")
+#Helper columns
 #c.execute("ALTER TABLE sets ADD loser_id text")
 c.execute("UPDATE sets SET loser_id= CASE WHEN winner_id=p1_id THEN p2_id ELSE p1_id END")
 c.execute("ALTER TABLE sets ADD score_diff integer")
 c.execute("UPDATE sets SET score_diff= ABS(p1_score-p2_score)")
 
-"""
-c.execute("SELECT player_id FROM players WHERE tag='Chillindude'")
-print(c.fetchone())
-
-c.execute("SELECT * FROM sets WHERE winner_id='1493' AND loser_id='1000'")
-players=c.fetchall()
-print(players)
-"""
-c.execute("SELECT tag FROM players")
-players=np.unique(c.fetchall())
+#TODO refactor more efficiently
+c.execute("""SELECT p1.tag,p2.tag
+    FROM sets s
+    JOIN players p1 ON p1.player_id=s.winner_id 
+    JOIN players p2 ON p2.player_id=s.loser_id
+    JOIN tournament_info t ON s.tournament_key=t.key
+    WHERE t.season=20""")
+temp=[x for y in c for x in y]
+players=np.unique(temp)
 print("There are %i unique players" %len(players))
 
-#How many times has player x beaten player y and game diff without dqs
+
+#How many times has player x beaten player y and game diff without dqs in season
 c.execute("""SELECT p1.tag,p2.tag,COUNT(s.winner_id),SUM(s.score_diff)
     FROM sets s
     JOIN players p1 ON p1.player_id=s.winner_id 
     JOIN players p2 ON p2.player_id=s.loser_id
-    WHERE p1_score <> -1 AND p2_score <> -1
+    JOIN tournament_info t ON s.tournament_key=t.key
+    WHERE p1_score <> -1 AND p2_score <> -1 AND t.season=20
     GROUP BY s.winner_id, s.loser_id""") 
 head2Head=c.fetchall()
 print("There are %i h2h records" %len(head2Head))
-
 
 #Adjacency Matrix
 def adjancency():
     adjMatrix= pd.DataFrame(np.zeros((len(players), len(players))), index=players, columns=players)
     for record in head2Head:
-        adjMatrix.at[record[0],record[1]]=record[2]
-    #Saving the matrix to disk is a ~2 GB endeavor
-    #adjMatrix.to_csv("out.csv")
+        adjMatrix.at[record[0],record[1]]+=record[2]
     return adjMatrix
 
 A=adjancency()
+kIn=A.sum(axis=1)
+kOut=A.sum(axis=0)
 
+
+#alpha must be less than the inverse of the max eigenvalue
+#For Season 19, the max eigenvalue is 16.075
+#For season 20, it was 0, is it acyclic?
+print("Waiting on eigenvalues")
+#alpha=.8/max(np.linalg.eigvals(A))
+#print(alpha)
+alpha=.8/16.075
+
+print("No more eigenvalues")
+
+w= np.dot(np.linalg.inv(np.identity(len(players))-alpha*A),kIn)
+l= np.dot(np.linalg.inv(np.identity(len(players))-alpha*np.transpose(A)),kOut)
+
+s=w-l
+scores=pd.DataFrame(s, index=players,columns=["Score"])
+
+scores.sort_values(by="Score", ascending=False).to_csv("2020ranking.csv")
 
 c.close()
 conn.close()
-
-#g=ig.Graph.Tree(12,2)
-#ig.plot(g)
